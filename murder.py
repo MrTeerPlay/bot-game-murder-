1f6151b1726d1ef641a3fc3d013d5fddfa64ea9c
MrTeerPlay <andriyvin08@gmail.com>
2024-12-07 07:52:13 -0800

    друга спроба добавити чергу

--- murder.py
+++ murder.py
@@ -1,7 +1,9 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters import Command
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, BotCommand
+from aiogram.types import Message, CallbackQuery, BotCommand # type: ignore
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 
 bot = Bot(token='8187696469:AAHR8LCtDK6CCh9-vDpNDOAuFa3p98hPxmU')
 
@@ -35,21 +37,44 @@ def active_or_inactive():
     if queue_active==True:
         return InlineKeyboardButton(text="Вийти", callback_data="leave_queue")
     else:
-        return InlineKeyboardButton(text="Приєднатися", callback_data="callback_data")
+        return InlineKeyboardButton(text="Приєднатися", callback_data="join_queue")
 
 @router.message(Command("startgame"))
 async def send_game(message: Message):
     button = InlineKeyboardButton(text="Приєднатися", callback_data="callback_data")
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button]])
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[active_or_inactive()]])
     await message.answer("Кімната вже готова, приєднуйся до гри!!!", reply_markup=keyboard)
 
 @router.callback_query(lambda callback_query: callback_query.data == "callback_data")
 async def callback(query: CallbackQuery):
     await query.answer("")
 
+@router.callback_query(lambda c: c.data in ['join_queue', 'leave_queue'])
+async def process_callback(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    global queue_active
 
-#####################################################################################################
-####################################################################################################################################################
+    if callback_query.data == "join_queue":
+        if user_id not in queue:
+            queue.append(user_id)
+            queue_active = True  # Черга стає активною
+            await bot.answer_callback_query(callback_query.id, text="Ви приєдналися до черги!")
+        else:
+            await bot.answer_callback_query(callback_query.id, text="Ви вже в черзі!")
+    
+    elif callback_query.data == "leave_queue":
+        if user_id in queue:
+            queue.remove(user_id)
+            if not queue:  # Якщо черга порожня, вона більше не активна
+                queue_active = False
+            await bot.answer_callback_query(callback_query.id, text="Ви вийшли з черги!")
+        else:
+            await bot.answer_callback_query(callback_query.id, text="Ви не в черзі!")
+
+    # Оновлюємо кнопку
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[active_or_inactive()]])
+
+
 
 
 async def main():
